4038992658727ff9c0ee11b6577dd3dbb41f66f7
Stella Tsiona <32960647+Stellou88@users.noreply.github.com>
2017-10-20 16:12:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello");
+alert("A R E   Y O U   H U N G R Y  ?");
+
+
+
+var menu = { first:"Recipies", second:"Videos", third:"Cooking Tips"};
+
+var menu = ["Recipies", "Videos", "Cooking Tips"];
+document.getElementById("demo").innerHTML = menu;
